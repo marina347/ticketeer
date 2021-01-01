a4ca2f75494ccc61e1584d1b0ae7e4dc28203924
marina347 <marinabulic1@gmail.com>
2021-01-01 19:03:57 +0100

    Removing non existing import

--- client/src/components/user-view/user-view.component.jsx
+++ client/src/components/user-view/user-view.component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./user-view.styles.scss";
 import SignOut from "../sign-out/sign-out.component";
 import UserIconBig from "../user-icon/user-icon-big/user-icon-big.component";
-import { ReactComponent as ProfileIcon } from "../../assets/svg/profile.svg";
 
 const UserView = ({ name, url, opened, email, onViewClose }) => {
   if (opened) {
