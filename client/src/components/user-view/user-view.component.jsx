cc7b575fd04ca9e0d16a278f1d612aa9dbf8ba56
marina347 <marinabulic1@gmail.com>
2021-01-01 19:03:24 +0100

    Chanhes in user view styles

--- client/src/components/user-view/user-view.component.jsx
+++ client/src/components/user-view/user-view.component.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import "./user-view.styles.scss";
 import SignOut from "../sign-out/sign-out.component";
 import UserIconBig from "../user-icon/user-icon-big/user-icon-big.component";
+import { ReactComponent as ProfileIcon } from "../../assets/svg/profile.svg";
 
 const UserView = ({ name, url, opened, email, onViewClose }) => {
   if (opened) {
     return (
       <div className="user-view">
-        <button onClick={onViewClose} class="user-view__close">&times;</button>
+        <button onClick={onViewClose} class="user-view__close">
+          &times;
+        </button>
         <UserIconBig name={name} url={url} />
-        <span className="user-view__name">{name}</span>
-        <span className="user-view__email u-margin-bottom-small">{email}</span>
+        <p className="user-view__name">{name}</p>
+        <p className="user-view__email u-margin-bottom-small">{email}</p>
         <SignOut />
       </div>
     );
