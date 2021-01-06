375a72a0a0e253e133f2379edcb5a21ed0bfbeda
marina347 <marinabulic1@gmail.com>
2021-01-06 17:06:02 +0100

    Added tests for not found

--- client/src/components/not-found/not-found.component.jsx
+++ client/src/components/not-found/not-found.component.jsx
@@ -4,14 +4,15 @@ import { withRouter } from "react-router-dom";
 import FormButton from "../form-button/form-button.component";
 import "./not-found.styles.scss";
 
-const NotFound = ({ history }) => {
+export const NotFound = ({ history }) => {
   return (
-    <div className="not-found">
+    <div id="not_found" className="not-found">
       <h1 className="not-found__heading">404</h1>
       <p className="not-found__details">
         Sorry, we couldn't find the page you were looking for.
       </p>
       <FormButton
+        id="go_home"
         onClick={() => history.push("/home")}
         className="btn btn-main btn-main--white u-animation-none"
       >
