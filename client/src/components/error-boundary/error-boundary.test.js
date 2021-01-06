be88d2e28c4d0f5476b4a91611ebbaf0f69d1f9f
marina347 <marinabulic1@gmail.com>
2021-01-06 16:51:13 +0100

    Updates to error boundary and memebr test

--- client/src/components/error-boundary/error-boundary.test.js
+++ client/src/components/error-boundary/error-boundary.test.js
@@ -2,11 +2,19 @@ import React from "react";
 import { shallow } from "enzyme";
 import ErrorBoundary from "./error-boundary.component";
 
+function Something() {
+  return null;
+}
+
 describe("ErrorBoundary component", () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<ErrorBoundary />);
+    wrapper = shallow(
+      <ErrorBoundary>
+        <Something />
+      </ErrorBoundary>
+    );
   });
 
   it("Should render ErrorBoundary component", () => {
@@ -14,11 +22,11 @@ describe("ErrorBoundary component", () => {
   });
 
   it("Should  render error container if there is error", () => {
-    wrapper.setState({ hasError: true });
+    const error = new Error("error");
+    wrapper.find(Something).simulateError(error);
     expect(wrapper.find('[id="error"]').exists()).toBe(true);
   });
   it("Should not render error container if there is no error", () => {
     expect(wrapper.find('[id="error"]').exists()).toBe(false);
   });
-  
 });
--- client/src/components/member/member.test.js
+++ client/src/components/member/member.test.js
@@ -3,5 +3,7 @@ import { shallow } from "enzyme";
 import Member from "./member.component";
 
 it("Should render Member component", () => {
-  expect(shallow(<Member name={"name"} />)).toMatchSnapshot();
+  expect(
+    shallow(<Member name={"name"} email={"email"} picture={"picture"} />)
+  ).toMatchSnapshot();
 });
