d7edabe781bcb5fcc7390ac4efabf7e79f62470e
marina347 <marinabulic1@gmail.com>
2021-01-09 20:02:37 +0100

    Converted app to functional component and added test. Removed lazy imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -6,38 +6,32 @@ import "react-toastify/dist/ReactToastify.css";
 import { Header } from "./components/header/header.component";
 import ErrorBoundary from "./components/error-boundary/error-boundary.component";
 import NotFound from "./components/not-found/not-found.component";
-import Spinner from "./components/spinner/spinner.component";
 import Footer from "./components/footer/footer.component";
+import HomePage from "./pages/home/home.component";
+import SignInPage from "./pages/sign-in/sign-in.component";
 import "./App.scss";
 
-const HomePage = lazy(() => import("./pages/home/home.component"));
-const SignInPage = lazy(() => import("./pages/sign-in/sign-in.component"));
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <Header />
-        <ErrorBoundary>
-          <Suspense fallback={<Spinner />}>
-            <Switch>
-              <Redirect exact from="/" to="/home" />
-              <Route path="/home" component={HomePage} />
-              <Route exact path="/login" component={SignInPage} />
-              <Route component={NotFound} />
-            </Switch>
-          </Suspense>
-        </ErrorBoundary>
-        <Footer />
-        <ToastContainer
-          style={{ fontSize: "1.6rem" }}
-          bodyClassName="toast-body"
-          position="bottom-right"
-          hideProgressBar
-        />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <ErrorBoundary>
+        <Switch>
+          <Redirect exact from="/" to="/home" />
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/login" component={SignInPage} />
+          <Route component={NotFound} />
+        </Switch>
+      </ErrorBoundary>
+      <Footer />
+      <ToastContainer
+        style={{ fontSize: "1.6rem" }}
+        bodyClassName="toast-body"
+        position="bottom-right"
+        hideProgressBar
+      />
+    </div>
+  );
+};
 
 export default App;
